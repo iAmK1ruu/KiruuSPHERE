98db1778c717448f73ee07d721b223266be7a6d5
iAmK1ruu <23ln5567_ms@psu.edu.ph>
2024-10-04 23:11:12 +0800

    Added pending project files

---
+++ src/main/java/com/kiruu/kiruusphere/RequestWeatherData.java
@@ -0,0 +1,11 @@
+package com.kiruu.kiruusphere;
+
+public class RequestWeatherData {
+    public static String[] getResponse(String urlParam) {
+
+
+        String[] noResponse = {"NO_RESPONSE"};
+        return noResponse;
+    }
+
+}
